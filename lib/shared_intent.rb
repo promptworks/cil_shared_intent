e7ee5ffba167035a29ca4108776a6490c7b3289a
Jeff Deville <jeffdeville@gmail.com>
2016-03-01 15:36:27 -0500

    Initial commit

--- lib/shared_intent.rb
+++ lib/shared_intent.rb
@@ -1,5 +1,160 @@
+require 'logger'
+require 'json'
 require "shared_intent/version"
 
 module SharedIntent
-  # Your code goes here...
+  def self.included(klass)
+    klass.extend ClassMethods
+  end
+
+  module ClassMethods
+    attr_accessor :routes
+
+    def run!
+      new.run
+    end
+
+    # Because I don't want to try and put messages on the queue until the class is
+    # instantiated.
+    def register_route(intents:, data_types:)
+      @routes ||= []
+      routes << { intents: intents, data_types: data_types }
+    end
+
+    def exchange_name
+      "#{self.name}Exchange"
+    end
+
+    def queue_name
+      "#{self.name}Queue"
+    end
+  end
+
+  def run
+    subscribe_to_queue do |delivery_info, metadata, payload|
+      response = _handle_message(delivery_info, metadata, payload)
+      publish response
+    end
+    subscribe_to_queue(&:_handle_message)
+    register_routes
+
+    loop do
+      Thread.pass; sleep 1
+    end
+  rescue Interrupt
+    puts "\nExiting"
+    exit
+  end
+
+  def kill
+    connection.close if @_connection
+  end
+
+
+  def _handle_message(delivery_info, metadata, payload)
+    logger.info from_queue: payload
+    fail "You must define handle_message" unless respond_to? :handle_message
+    message = JSON.parse(payload) || {}
+    response = handle_message(delivery_info, metadata, message)
+    response = ensure_routing_maintained message, response
+    logger.info to_queue: response
+    response
+  end
+
+  def ensure_routing_maintained(message, response)
+    assert_valid_response response
+    response.merge("routing" => message.fetch("routing"))
+  end
+
+  def assert_valid_response(response)
+    if response.nil?
+      logger.error "No response provided"
+      fail "No response provided"
+    end
+    response.keys.each do |key|
+      response[key.to_s] = response.delete(key)
+    end
+    intent_keys = %w(intents data_types data)
+    if not response.respond_to? :key?
+      fail "Response must be a routable intent hash with: #{intent_keys}"
+    end
+    unless (response.keys & intent_keys).length == intent_keys.length
+      fail "Response must be a routable intent hash. Missing: #{intent_keys - response.keys}"
+    end
+  end
+
+  def subscribe_to_queue(&block)
+    queue.subscribe do |delivery_info, metadata, payload|
+      block.call(delivery_info, metadata, payload)
+    end
+  end
+
+  def publish(payload)
+    if payload.nil?
+      logger.debug("Not publishing nil payload")
+      return
+    end
+    string = JSON.dump(payload)
+    router_exchange.publish(string)
+  end
+
+  def register_routes
+    self.class.routes.each do |route|
+      router_exchange.publish(JSON.dump(route.merge(exchange: self.class.exchange_name)), routing_key: "add_route")
+    end
+  end
+
+  def connection
+    return @_connection if @_connection
+    @_connection = Bunny.new({
+      host:      ENV['RABBIT_HOSTNAME'],
+      port:      ENV['RABBIT_PORT'],
+      ssl:       false,
+      vhost:     '/',
+      user:      'guest',
+      pass:      'guest',
+      heartbeat: :server, # will use RabbitMQ setting
+      frame_max: 131072,
+      auth_mechanism: 'PLAIN'
+    })
+
+    begin
+      last_exception = nil
+      Timeout::timeout(20) do
+        begin
+          @_connection.start
+        rescue StandardError => e
+          logger.error "RabbitMQ connection failed. Retrying."
+          last_exception = e
+          sleep 0.5
+          retry
+        end
+      end
+    rescue Timeout::Error => timeout
+      raise(last_exception || timeout)
+    end
+
+    @_connection
+  end
+
+  def channel
+    @_channel ||= connection.create_channel
+  end
+
+  def router_exchange
+    @_router_exchange ||= channel.topic("router_exchange", auto_delete: false, durable: true)
+  end
+
+  def queue
+    return @_queue if @_queue
+    exchange = channel.topic(self.class.exchange_name, auto_delete: false, durable: true)
+    @_queue = channel.queue(self.class.queue_name, auto_delete: true)
+    logger.info queue: self.class.queue_name
+    @_queue.bind(exchange)
+    @_queue
+  end
+
+  def logger
+    @logger ||= ::Logger.new(STDOUT)
+  end
 end
--- spec/shared_intent_spec.rb
+++ spec/shared_intent_spec.rb
@@ -5,7 +5,105 @@ describe SharedIntent do
     expect(SharedIntent::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  class TestSharedIntents
+    include SharedIntent
+    register_route intents: "chime.testing", data_types: "chime.string"
+  end
+
+  let(:routable_response) do
+    {
+      "intents" => "chime.ActionIntent",
+      "data_types" => "chime.string",
+      "data" => "This is a response"
+    }
+  end
+  subject { TestSharedIntents.new }
+
+  describe "when included" do
+    %i(run publish register_routes connection channel router_exchange queue logger).each do |method|
+      it "responds to #{method}" do
+        expect(subject.respond_to? method).to be true
+      end
+    end
+
+    it "lets you register routes" do
+      expect(TestSharedIntents.respond_to? :register_route).to be true
+    end
+
+    it "stored the appropriate routes on the class" do
+      expected_routes = [
+        { intents: "chime.testing", data_types: "chime.string" }
+      ]
+      expect(TestSharedIntents.routes).to match_array(expected_routes)
+    end
+
+    it "attempts to register the routes via the intent router" do
+      dbl = double("router_exchange")
+      expect(dbl).to receive(:publish).once.with({intents: "chime.testing", data_types: "chime.string", exchange: "TestSharedIntentsExchange" })
+      allow(subject).to receive(:router_exchange).and_return dbl
+      subject.register_routes
+    end
+  end
+
+  describe "ensure_routing_maintained" do
+    let(:message) { { "routing" => {} } }
+    let(:response) { routable_response }
+
+    context "no response provided" do
+      let(:response) { nil }
+      it "fails" do
+        expect{subject.ensure_routing_maintained(message, response)}.
+          to raise_exception("No response provided")
+      end
+    end
+
+    context "no routing provided" do
+      let(:message) { {} }
+      it "fails" do
+        expect{subject.ensure_routing_maintained(message, response)}.
+          to raise_exception(KeyError, "key not found: \"routing\"")
+      end
+    end
+
+    context "response is not a hash" do
+      let(:response) { "blah blah" }
+      it "fails" do
+        expect{subject.ensure_routing_maintained(message, response)}.
+         to raise_exception('Response must be a routable intent hash with: ["intents", "data_types", "data"]')
+      end
+    end
+
+    context "response is not a routable hash" do
+      let(:response) { { "data" => "not a routable hash" } }
+      it "fails" do
+        expect{subject.ensure_routing_maintained(message, response)}.
+          to raise_exception('Response must be a routable intent hash. Missing: ["intents", "data_types"]')
+      end
+    end
+
+    context "all data normal", :focus do
+      it "merges the routing info to the response" do
+        new_response = subject.ensure_routing_maintained(message, response)
+        expect(new_response).to match(response.merge(message))
+      end
+    end
+  end
+
+  describe "_handle_message" do
+    let(:delivery_info) { "delivery_info" }
+    let(:metadata) { "metadata" }
+    let(:hash_payload) { {"this" => "is the data", "routing" => {} } }
+    let(:string_payload) { JSON.dump(hash_payload) }
+
+    it 'fails if handle_message is not defined' do
+      expect{subject._handle_message(nil, nil, nil)}.to raise_exception("You must define handle_message")
+    end
+
+    it "parses the payload into a hash" do
+      expect(subject).to receive(:handle_message).
+                         with(delivery_info, metadata, hash_payload).
+                         and_return(routable_response)
+      response = subject._handle_message(delivery_info, metadata, string_payload)
+    end
   end
 end
